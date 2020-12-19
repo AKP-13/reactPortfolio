120c6a451e5acbd5fae36b3babd8c312727bfc36
AKP-13 <alexpeirson@hotmail.com>
2020-12-19 18:53:59 +0000

    Export navbar, home, photography, projects, about, contact components

---
+++ src/Components/index.js
@@ -0,0 +1,6 @@
+export { default as Navbar } from "./Navbar";
+export { default as Home } from "./Home";
+export { default as Photography } from "./Photography";
+export { default as Projects } from "./Projects";
+export { default as About } from "./About";
+export { default as Contact } from "./Contact";
