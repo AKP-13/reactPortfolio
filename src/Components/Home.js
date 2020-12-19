577db154fb5622b2f0480dc5fb2713ac16029f20
AKP-13 <alexpeirson@hotmail.com>
2020-12-19 17:19:04 +0000

    Add name on homepage

---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "../styles/home.css";
+
+class Home extends Component {
+    render() {
+        return (
+            <>
+                <h1>Alex Peirson</h1>
+            </>
+        );
+    }
+}
+
+export default Home;
