1ccddfd98bb6ed13ac77e456767dda5cf76cb212
AKP-13 <alexpeirson@hotmail.com>
2020-12-19 17:09:29 +0000

    Add navbar and Home route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
 
 class App extends React.Component {
     render() {
         return (
-            <main>
-                <h1>React Portfolio!</h1>
-            </main>
+            <>
+                <Navbar />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                </Switch>
+            </>
         );
     }
 }
