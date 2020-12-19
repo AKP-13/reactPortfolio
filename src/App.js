83d0c0da6a6c69de7ccb372798462018d5a6009f
AKP-13 <alexpeirson@hotmail.com>
2020-12-19 14:15:52 +0000

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends React.Component {
+    render() {
+        return (
+            <main>
+                <h1>React Portfolio!</h1>
+            </main>
+        );
+    }
+}
+
+export default App;
