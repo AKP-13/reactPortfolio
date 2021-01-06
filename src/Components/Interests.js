bdcb358de8161e44da92acc9b763ed201b790d25
AKP-13 <alexpeirson@hotmail.com>
2021-01-06 15:43:19 +0000

    Add interests tab template

---
+++ src/Components/Interests.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Interests extends Component {
+    render() {
+        return (
+            <>
+                <p>Interests text goes here</p>
+            </>
+        );
+    }
+}
+
+export default Interests;
