7c2564c1b234e87126daab8d2634ac68fa278abe
AKP-13 <alexpeirson@hotmail.com>
2021-01-06 15:45:04 +0000

    Add background, interests, goals components

--- src/Components/index.js
+++ src/Components/index.js
@@ -4,3 +4,6 @@ export { default as Photography } from "./Photography";
 export { default as Projects } from "./Projects";
 export { default as About } from "./About";
 export { default as Contact } from "./Contact";
+export { default as Background } from "./Background";
+export { default as Interests } from "./Interests";
+export { default as Goals } from "./Goals";
