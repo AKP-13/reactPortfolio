e936ddec0ff3647cdaff62db8c9e447491ae64df
AKP-13 <alexpeirson@hotmail.com>
2021-01-06 17:28:46 +0000

    Add image

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import AboutTabs from "./AboutTabs";
+// import Image from "react-bootstrap/Image";
+import Figure from "react-bootstrap/Figure";
 import "../styles/about.css";
 
 class About extends Component {
@@ -11,6 +13,15 @@ class About extends Component {
         return (
             <>
                 <h1 id="about-me-title">Hi, I'm Alex</h1>
+                <Figure id="about-figure">
+                    <Figure.Image
+                        id="about-figure-image"
+                        width={300}
+                        height={300}
+                        alt="Me!"
+                        src="https://res.cloudinary.com/de8a23w1z/image/upload/v1602251966/portfolio/IMG_1698_emyb0u.png"
+                    />
+                </Figure>
                 <AboutTabs />
             </>
         );
