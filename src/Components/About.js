f3a5c49ccb34cf5fd94e3d8c3d0476e0d3634ab3
AKP-13 <alexpeirson@hotmail.com>
2021-01-18 15:57:32 +0000

    Begin implementing blurry image load

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,33 +1,47 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import AboutTabs from "./AboutTabs";
 import Container from "react-bootstrap/Container";
 import "../styles/about.css";
 
-class About extends Component {
-    componentDidMount() {
+function About() {
+    const [isLoaded, setIsLoaded] = React.useState(false);
+
+    useEffect(() => {
         document.body.style.background = "#fff";
-    }
+    });
 
-    render() {
-        return (
-            <>
-                <Container fluid>
-                    <div className="row" id="about-heading">
-                        <h1 id="about-me-title">Hi, I'm Alex </h1>
-                        <img
-                            id="about-figure-image"
-                            alt="Me!"
-                            src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1602251966/portfolio/IMG_1698_emyb0u.png"
-                        />
-                    </div>
+    return (
+        <>
+            <Container fluid>
+                <div className="row" id="about-heading">
+                    <h1 id="about-me-title">Hi, I'm Alex </h1>
+                    <img
+                        className="thumb"
+                        id="about-figure-image"
+                        alt="Me!"
+                        src="https://res.cloudinary.com/de8a23w1z/image/upload/c_scale,h_468,q_10,f_auto/v1610983198/portfolio/headshotB_W_byq2l1.png"
+                        style={{
+                            display: isLoaded ? "none" : "inline-block",
+                        }}
+                    />
+                    <img
+                        onLoad={() => {
+                            setIsLoaded(true);
+                        }}
+                        className="full"
+                        id="about-figure-image"
+                        style={{ opacity: isLoaded ? 1 : 0 }}
+                        alt="Me!"
+                        src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1602251966/portfolio/IMG_1698_emyb0u.png"
+                    />
+                </div>
 
-                    <div id="aboutAlex">
-                        <AboutTabs />
-                    </div>
-                </Container>
-            </>
-        );
-    }
+                <div id="aboutAlex">
+                    <AboutTabs />
+                </div>
+            </Container>
+        </>
+    );
 }
 
 export default About;
--- src/Components/PhotoCarousel.js
+++ src/Components/PhotoCarousel.js
@@ -15,165 +15,176 @@ import flagForVietnam from "@iconify-icons/emojione/flag-for-vietnam";
 import flagForLaos from "@iconify-icons/emojione/flag-for-laos";
 import flagForThailand from "@iconify-icons/emojione/flag-for-thailand";
 
-class PhotoCarousel extends Component {
-    render() {
-        return (
-            <>
-                <Carousel>
-                    <Carousel.Item interval={5000}>
-                        <img
-                            className="d-block w-100 carousel-photo"
-                            src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1610754700/portfolio/F1824CE4-B1A6-42AF-9DA8-B089111F9BD1_lwfgkg.jpg"
-                            alt="England"
-                        />
-                        <Carousel.Caption>
-                            <h3 className="countryNames">
-                                <InlineIcon icon={flagEngland} /> England
-                            </h3>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item interval={5000}>
-                        <img
-                            className="d-block w-100 carousel-photo"
-                            src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1610729753/portfolio/IMG_0531_enftpw.jpg"
-                            alt="Spain"
-                        />
-                        <Carousel.Caption>
-                            <h3 className="countryNames">
-                                <InlineIcon icon={flagForSpain} /> Spain
-                            </h3>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item interval={5000}>
-                        <img
-                            className="d-block w-100 carousel-photo"
-                            src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1610732012/portfolio/IMG_1907_oa8clh.jpg"
-                            alt="France"
-                        />
-                        <Carousel.Caption>
-                            <h3 className="countryNames">
-                                <InlineIcon icon={flagForFrance} /> France
-                            </h3>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item interval={5000}>
-                        <img
-                            className="d-block w-100 carousel-photo"
-                            src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1610729786/portfolio/IMG_2995_v2gfy3.jpg"
-                            alt="Croatia"
-                        />
-                        <Carousel.Caption>
-                            <h3 className="countryNames">
-                                <InlineIcon icon={flagForCroatia} /> Croatia
-                            </h3>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item interval={5000}>
-                        <img
-                            className="d-block w-100 carousel-photo"
-                            src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1610732074/portfolio/IMG_0465_imjmcw.jpg"
-                            alt="Lithuania"
-                        />
-                        <Carousel.Caption>
-                            <h3 className="countryNames">
-                                <InlineIcon icon={flagForLithuania} /> Lithuania
-                            </h3>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item interval={5000}>
-                        <img
-                            className="d-block w-100 carousel-photo"
-                            src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1610732113/portfolio/IMG_3950_v4e22o.jpg"
-                            alt="Malaysia"
-                        />
-                        <Carousel.Caption>
-                            <h3 className="countryNames">
-                                <InlineIcon icon={flagForMalaysia} /> Malaysia
-                            </h3>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item interval={5000}>
-                        <img
-                            className="d-block w-100 carousel-photo"
-                            src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1600948599/portfolio/IMG_2006_sknklq.jpg"
-                            alt="Singapore"
-                        />
+function PhotoCarousel() {
+    const [isLoaded, setIsLoaded] = React.useState(false);
+    return (
+        <>
+            <Carousel>
+                <Carousel.Item interval={30000}>
+                    <img
+                        className="w-100 carousel-photo thumb"
+                        src="https://res.cloudinary.com/de8a23w1z/image/upload/c_scale,h_592,q_10/v1610984558/portfolio/englandPhoto_iiuiup.jpg"
+                        alt="England"
+                        style={{
+                            display: isLoaded ? "none" : "inline-block",
+                        }}
+                    />
+                    <img
+                        onLoad={() => {
+                            setIsLoaded(true);
+                        }}
+                        style={{ opacity: isLoaded ? 1 : 0 }}
+                        className="d-block w-100 carousel-photo full"
+                        src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1610754700/portfolio/F1824CE4-B1A6-42AF-9DA8-B089111F9BD1_lwfgkg.jpg"
+                        alt="England"
+                    />
+                    <Carousel.Caption>
+                        <h3 className="countryNames">
+                            <InlineIcon icon={flagEngland} /> England
+                        </h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={5000}>
+                    <img
+                        className="d-block w-100 carousel-photo"
+                        src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1610729753/portfolio/IMG_0531_enftpw.jpg"
+                        alt="Spain"
+                    />
+                    <Carousel.Caption>
+                        <h3 className="countryNames">
+                            <InlineIcon icon={flagForSpain} /> Spain
+                        </h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={5000}>
+                    <img
+                        className="d-block w-100 carousel-photo"
+                        src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1610732012/portfolio/IMG_1907_oa8clh.jpg"
+                        alt="France"
+                    />
+                    <Carousel.Caption>
+                        <h3 className="countryNames">
+                            <InlineIcon icon={flagForFrance} /> France
+                        </h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={5000}>
+                    <img
+                        className="d-block w-100 carousel-photo"
+                        src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1610729786/portfolio/IMG_2995_v2gfy3.jpg"
+                        alt="Croatia"
+                    />
+                    <Carousel.Caption>
+                        <h3 className="countryNames">
+                            <InlineIcon icon={flagForCroatia} /> Croatia
+                        </h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={5000}>
+                    <img
+                        className="d-block w-100 carousel-photo"
+                        src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1610732074/portfolio/IMG_0465_imjmcw.jpg"
+                        alt="Lithuania"
+                    />
+                    <Carousel.Caption>
+                        <h3 className="countryNames">
+                            <InlineIcon icon={flagForLithuania} /> Lithuania
+                        </h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={5000}>
+                    <img
+                        className="d-block w-100 carousel-photo"
+                        src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1610732113/portfolio/IMG_3950_v4e22o.jpg"
+                        alt="Malaysia"
+                    />
+                    <Carousel.Caption>
+                        <h3 className="countryNames">
+                            <InlineIcon icon={flagForMalaysia} /> Malaysia
+                        </h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={5000}>
+                    <img
+                        className="d-block w-100 carousel-photo"
+                        src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1600948599/portfolio/IMG_2006_sknklq.jpg"
+                        alt="Singapore"
+                    />
 
-                        <Carousel.Caption>
-                            <h3 className="countryNames">
-                                <InlineIcon icon={flagForSingapore} /> Singapore
-                            </h3>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item interval={5000}>
-                        <img
-                            className="d-block w-100 carousel-photo"
-                            src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1610732072/portfolio/IMG_3095_h5ffgn.jpg"
-                            alt="Indonesia"
-                        />
+                    <Carousel.Caption>
+                        <h3 className="countryNames">
+                            <InlineIcon icon={flagForSingapore} /> Singapore
+                        </h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={5000}>
+                    <img
+                        className="d-block w-100 carousel-photo"
+                        src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1610732072/portfolio/IMG_3095_h5ffgn.jpg"
+                        alt="Indonesia"
+                    />
 
-                        <Carousel.Caption>
-                            <h3 className="countryNames">
-                                <InlineIcon icon={flagForIndonesia} /> Indonesia
-                            </h3>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item interval={5000}>
-                        <img
-                            className="d-block w-100 carousel-photo"
-                            src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1608314600/portfolio/IMG_4328_uz4xxp.jpg"
-                            alt="Cambodia"
-                        />
+                    <Carousel.Caption>
+                        <h3 className="countryNames">
+                            <InlineIcon icon={flagForIndonesia} /> Indonesia
+                        </h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={5000}>
+                    <img
+                        className="d-block w-100 carousel-photo"
+                        src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1608314600/portfolio/IMG_4328_uz4xxp.jpg"
+                        alt="Cambodia"
+                    />
 
-                        <Carousel.Caption>
-                            <h3 className="countryNames">
-                                <InlineIcon icon={flagForCambodia} /> Cambodia
-                            </h3>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item interval={5000}>
-                        <img
-                            className="d-block w-100 carousel-photo"
-                            src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1599147896/portfolio/IMG_5418_uecow9.jpg"
-                            alt="Vietnam"
-                        />
+                    <Carousel.Caption>
+                        <h3 className="countryNames">
+                            <InlineIcon icon={flagForCambodia} /> Cambodia
+                        </h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={5000}>
+                    <img
+                        className="d-block w-100 carousel-photo"
+                        src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1599147896/portfolio/IMG_5418_uecow9.jpg"
+                        alt="Vietnam"
+                    />
 
-                        <Carousel.Caption>
-                            <h3 className="countryNames">
-                                <InlineIcon icon={flagForVietnam} /> Vietnam
-                            </h3>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item interval={5000}>
-                        <img
-                            className="d-block w-100 carousel-photo"
-                            src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1610729772/portfolio/IMG_6767_x17saw.jpg"
-                            alt="Laos"
-                        />
+                    <Carousel.Caption>
+                        <h3 className="countryNames">
+                            <InlineIcon icon={flagForVietnam} /> Vietnam
+                        </h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={5000}>
+                    <img
+                        className="d-block w-100 carousel-photo"
+                        src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1610729772/portfolio/IMG_6767_x17saw.jpg"
+                        alt="Laos"
+                    />
 
-                        <Carousel.Caption>
-                            <h3 className="countryNames">
-                                <InlineIcon icon={flagForLaos} /> Laos
-                            </h3>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item interval={5000}>
-                        <img
-                            className="d-block w-100 carousel-photo"
-                            src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1610732035/portfolio/IMG_7171_v4jvv3.jpg"
-                            alt="Thailand"
-                        />
+                    <Carousel.Caption>
+                        <h3 className="countryNames">
+                            <InlineIcon icon={flagForLaos} /> Laos
+                        </h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={5000}>
+                    <img
+                        className="d-block w-100 carousel-photo"
+                        src="https://res.cloudinary.com/de8a23w1z/image/upload/q_auto:best,f_auto/v1610732035/portfolio/IMG_7171_v4jvv3.jpg"
+                        alt="Thailand"
+                    />
 
-                        <Carousel.Caption>
-                            <h3 className="countryNames">
-                                <InlineIcon icon={flagForThailand} /> Thailand
-                            </h3>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
-            </>
-        );
-    }
+                    <Carousel.Caption>
+                        <h3 className="countryNames">
+                            <InlineIcon icon={flagForThailand} /> Thailand
+                        </h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </>
+    );
 }
 
 export default PhotoCarousel;
