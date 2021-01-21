097b648001a8595c628b71fc12389c080da0527d
AKP-13 <alexpeirson@hotmail.com>
2021-01-21 13:07:38 +0000

    Restructre so that ul nolonger child of p tag

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -17,20 +17,20 @@ class Background extends Component {
                     lead me to apply to futureproof's coding bootcamp.
                     <br />
                     <br />
-                    The futureproof bootcamp gave me experience with
-                    <ul>
-                        <li>
-                            working individually on time restricted debugging
-                            assignments
-                        </li>
-                        <li>
-                            pair programming while using GitHub and version
-                            control best practices
-                        </li>
-                        <li>collaborating in groups on week-long projects</li>
-                        <li>daily stand-ups and weekly retros</li>
-                    </ul>
+                    The futureproof bootcamp gave me experience with:
                 </p>
+                <ul className="bulletPoints">
+                    <li>
+                        working individually on time restricted debugging
+                        assignments
+                    </li>
+                    <li>
+                        pair programming while using GitHub and version control
+                        best practices
+                    </li>
+                    <li>collaborating in groups on week-long projects</li>
+                    <li>daily stand-ups and weekly retros</li>
+                </ul>
             </>
         );
     }
