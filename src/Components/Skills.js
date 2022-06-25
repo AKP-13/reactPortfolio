45fc2c714cd4d23a9da6bbe51cbbf24bca75f252
AKP-13 <alexpeirson@hotmail.com>
2022-06-25 11:59:23 +0100

    Code syntax refactor

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,78 +1,32 @@
 import React, { Component } from "react";
 import "../styles/skills.css";
 
-class Skills extends Component {
-    render() {
-        return (
-            <p className="about-para icons">
-                <span
-                    className="iconify"
-                    data-icon="logos:html-5"
-                    data-inline="false"
-                ></span>
+const Skills = () => {
+    const DATA_ICONS = [
+        "logos:html-5",
+        "logos:css-3",
+        "logos:javascript",
+        "logos:react",
+        "logos:redux",
+        "logos:python",
+        "logos:flask",
+        "logos:django-icon",
+        "logos:postgresql",
+        "vscode-icons:file-type-mongo",
+        "simple-icons:express",
+    ];
 
+    return (
+        <p className="about-para icons">
+            {DATA_ICONS.map((icon) => (
                 <span
                     className="iconify"
-                    data-icon="logos:css-3"
+                    data-icon={icon}
                     data-inline="false"
                 ></span>
-
-                <span
-                    className="iconify"
-                    data-icon="logos:javascript"
-                    data-inline="false"
-                ></span>
-
-                <span
-                    className="iconify"
-                    data-icon="logos:react"
-                    data-inline="false"
-                ></span>
-
-                <span
-                    className="iconify"
-                    data-icon="logos:redux"
-                    data-inline="false"
-                ></span>
-
-                <span
-                    className="iconify"
-                    data-icon="logos:python"
-                    data-inline="false"
-                ></span>
-
-                <span
-                    className="iconify"
-                    data-icon="logos:flask"
-                    data-inline="false"
-                ></span>
-
-                <span
-                    className="iconify"
-                    data-icon="logos:django-icon"
-                    data-inline="false"
-                ></span>
-
-                <span
-                    className="iconify"
-                    data-icon="logos:postgresql"
-                    data-inline="false"
-                ></span>
-
-                <span
-                    className="iconify"
-                    data-icon="vscode-icons:file-type-mongo"
-                    data-inline="false"
-                ></span>
-
-                <span
-                    className="iconify"
-                    data-icon="simple-icons:express"
-                    data-inline="false"
-                ></span>
-            </p>
-        );
-    }
-}
+            ))}
+        </p>
+    );
+};
 
 export default Skills;
