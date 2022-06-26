10b9b1e12d2fc7dae94ce970572cf070df05ec58
AKP-13 <alexpeirson@hotmail.com>
2022-06-26 19:59:46 +0100

    Code syntax refactor

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -12,108 +12,100 @@ const Projects = () => {
         <main>
             <h1 id="projectsTitle">My work</h1>
             <div className="projects row row-cols-1 row-cols-md-2 row-cols-lg-3">
-                {PROJECT_CATALOGUE.map((project) => {
-                    return (
-                        <div className="col mb-4" key={project.title}>
-                            <div className="card h-100">
-                                <a href={project.projectLink}>
-                                    <img
-                                        src={project.imageLink}
-                                        className="card-img-top"
-                                        alt={project.imageAlt}
-                                    />
-                                </a>
-                                <div className="card-body">
-                                    <h5 className="card-title">
-                                        <a href={project.projectLink}>
-                                            {project.title}
-                                        </a>
-                                    </h5>
-                                    <span className="techLogos">
-                                        {project.techIcons.map((icon) => (
-                                            <span
-                                                className="iconify"
-                                                data-icon={icon}
-                                                key={`${project.title}-${icon}`}
-                                            ></span>
-                                        ))}
-                                    </span>
-                                    <hr />
-                                    <p className="card-text">
-                                        {project.description}
-                                    </p>
-                                </div>
-                                <div className="card-footer linksFooter">
-                                    <small className="text-muted links">
-                                        {project.footerIcons.map((icon) => {
-                                            if (
-                                                icon === "bx:bx-link-external"
-                                            ) {
-                                                return (
-                                                    <a
-                                                        href={
-                                                            project.projectLink
-                                                        }
-                                                        key={`${project.title}-${icon}`}
-                                                    >
-                                                        <span
-                                                            className="iconify"
-                                                            data-icon="bx:bx-link-external"
-                                                        ></span>
-                                                    </a>
-                                                );
-                                            } else if (
-                                                icon === "logos:github-icon"
-                                            ) {
-                                                return (
-                                                    <a
-                                                        href={
-                                                            project.gitHubLink
-                                                        }
-                                                        key={`${project.title}-${icon}`}
-                                                    >
-                                                        <span
-                                                            className="iconify"
-                                                            data-icon={icon}
-                                                        ></span>
-                                                    </a>
-                                                );
-                                            } else {
-                                                return (
+                {PROJECT_CATALOGUE.map((project) => (
+                    <div className="col mb-4" key={project.title}>
+                        <div className="card h-100">
+                            <a href={project.projectLink}>
+                                <img
+                                    src={project.imageLink}
+                                    className="card-img-top"
+                                    alt={project.imageAlt}
+                                />
+                            </a>
+                            <div className="card-body">
+                                <h5 className="card-title">
+                                    <a href={project.projectLink}>
+                                        {project.title}
+                                    </a>
+                                </h5>
+                                <span className="techLogos">
+                                    {project.techIcons.map((icon) => (
+                                        <span
+                                            className="iconify"
+                                            data-icon={icon}
+                                            key={`${project.title}-${icon}`}
+                                        ></span>
+                                    ))}
+                                </span>
+                                <hr />
+                                <p className="card-text">
+                                    {project.description}
+                                </p>
+                            </div>
+                            <div className="card-footer linksFooter">
+                                <small className="text-muted links">
+                                    {project.footerIcons.map((icon) => {
+                                        if (icon === "bx:bx-link-external") {
+                                            return (
+                                                <a
+                                                    href={project.projectLink}
+                                                    key={`${project.title}-${icon}`}
+                                                >
+                                                    <span
+                                                        className="iconify"
+                                                        data-icon="bx:bx-link-external"
+                                                    ></span>
+                                                </a>
+                                            );
+                                        } else if (
+                                            icon === "logos:github-icon"
+                                        ) {
+                                            return (
+                                                <a
+                                                    href={project.gitHubLink}
+                                                    key={`${project.title}-${icon}`}
+                                                >
                                                     <span
                                                         className="iconify"
                                                         data-icon={icon}
-                                                        key={`${project.title}-${icon}`}
                                                     ></span>
-                                                );
-                                            }
-                                        })}
-                                    </small>
-                                </div>
-                                <div className="card-footer">
-                                    <small className="text-muted">
-                                        Contributors:{" "}
-                                        {project.contributors.map(
-                                            (contributor, idx) => (
-                                                <div
-                                                    key={`${project.title}-${contributor.name}`}
-                                                >
-                                                    <a href={contributor.link}>
-                                                        {contributor.name}
-                                                    </a>
-                                                    {idx !==
-                                                        project.contributors
-                                                            .length -
-                                                            1 && <span>,</span>}
-                                                </div>
-                                            )
-                                        )}
-                                    </small>
-                                </div>
+                                                </a>
+                                            );
+                                        } else {
+                                            return (
+                                                <span
+                                                    className="iconify"
+                                                    data-icon={icon}
+                                                    key={`${project.title}-${icon}`}
+                                                ></span>
+                                            );
+                                        }
+                                    })}
+                                </small>
+                            </div>
+                            <div className="card-footer">
+                                <small className="text-muted">
+                                    Contributors:{" "}
+                                    {project.contributors.map(
+                                        (contributor, idx) => (
+                                            <div
+                                                key={`${project.title}-${contributor.name}`}
+                                            >
+                                                <a href={contributor.link}>
+                                                    {contributor.name}
+                                                </a>
+                                                {idx !==
+                                                    project.contributors
+                                                        .length -
+                                                        1 && <span>,</span>}
+                                            </div>
+                                        )
+                                    )}
+                                </small>
                             </div>
                         </div>
-                    );
-                })}
+                    </div>
+                ))}
             </div>
         </main>
     );
